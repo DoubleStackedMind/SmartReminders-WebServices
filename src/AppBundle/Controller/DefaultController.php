209a3ebc4b135ab4fdc359151a6badda5cefc027
Sami Asfouri <35670834+DoubleStackedMind@users.noreply.github.com>
2018-11-11 15:24:11 +0100

    Sami_WebService

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,13 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\user;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class DefaultController extends Controller
 {
@@ -17,5 +21,20 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
+    }
+    /**
+     * @Route("/", name="login")
+     */
+    public function LoginWebServiceFunctionAction (Request $request)
+    {
+        $us = new user();
+        $user = $this->getDoctrine()->getManager()
+            ->getRepository('AppBundle:User')->findOneBy(array(['username' => $request->get('username')]));
+        $us->setIduser($user);
+        $us->setUsername($user);
+        $us->setPwd($user);
+        $serializer = new Serializer([new ObjectNormalizer()]);
+        $formatted = $serializer->normalize($us);
+        return new JsonResponse($formatted);
     }
 }
